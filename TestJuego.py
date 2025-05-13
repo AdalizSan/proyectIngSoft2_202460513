20eca13c5764eff1c43a4135cbc2fb9323756b07
AdalizSan <adalizsantisteban@gmail.com>
2025-05-12 23:52:09 -0600

    incorporación de los test en caso de empate, gana la primera opción y pierde la primera opción.

---
+++ TestJuego.py
@@ -0,0 +1,37 @@
+import unittest
+from juego import compararOpciones
+
+class TestCompararOpciones(unittest.TestCase):
+    
+#empate
+    def testPiedra_Piedra(self):
+        self.assertEqual(compararOpciones('piedra', 'piedra'), 0)
+
+    def testPapel_Papel(self):
+        self.assertEqual(compararOpciones('papel', 'papel'), 0)
+
+    def testTijera_Tijera(self):
+        self.assertEqual(compararOpciones('tijera', 'tijera'), 0)
+
+# primero gana
+    def testPiedra_Tijera(self):
+        self.assertEqual(compararOpciones('piedra', 'tijera'), 1)
+
+    def testPapel_Piedra(self):
+        self.assertEqual(compararOpciones('papel', 'piedra'), 1)
+
+    def testTijera_Papel(self):
+        self.assertEqual(compararOpciones('tijera', 'papel'), 1)
+
+#pierde primera opción
+    def testPiedra_Papel(self):
+        self.assertEqual(compararOpciones('piedra', 'papel'), -1)
+
+    def testPapel_Tijera(self):
+        self.assertEqual(compararOpciones('papel', 'tijera'), -1)
+
+    def testTijeraPiedra(self):
+        self.assertEqual(compararOpciones('tijera', 'piedra'), -1)
+
+if __name__ == '__main__':
+    unittest.main()
--- juego.py
+++ juego.py
@@ -12,50 +12,52 @@ def compararOpciones(Op1, Op2):
         return 1
     return -1
 
-jugadasHumano = sys.argv[1:]
+if __name__=="__main__":
+    
+    jugadasHumano = sys.argv[1:]
 
-if len(jugadasHumano) != 3:
-    print('Por favor ingrese 3 opciones (piedra, papel o tijera).')
-    sys.exit()
-
-jugadasLimpias = []
-for jugada in jugadasHumano:
-    jugadaLimpia = jugada.strip().lower()
-    jugadasLimpias.append(jugadaLimpia)
-jugadasHumano = jugadasLimpias
-
-opcionesJuego = ['piedra', 'papel', 'tijera']
-for jugada in jugadasHumano:
-    if jugada not in opcionesJuego:
-        print('Opción inválida:', jugada)
+    if len(jugadasHumano) != 3:
+        print('Por favor ingrese 3 opciones (piedra, papel o tijera).')
         sys.exit()
 
-jugadasPrograma = []
-contador = 3
-while contador > 0:
-    numRandom = random.randrange(0, 3)
-    jugadasPrograma.append(opcionesJuego[numRandom])
-    contador -= 1
+    jugadasLimpias = []
+    for jugada in jugadasHumano:
+        jugadaLimpia = jugada.strip().lower()
+        jugadasLimpias.append(jugadaLimpia)
+    jugadasHumano = jugadasLimpias
 
-print('El programa elige:', jugadasPrograma)
+    opcionesJuego = ['piedra', 'papel', 'tijera']
+    for jugada in jugadasHumano:
+        if jugada not in opcionesJuego:
+            print('Opción inválida:', jugada)
+            sys.exit()
 
-contador = 0
-puntosHuman = 0
-puntosProgram = 0
+    jugadasPrograma = []
+    contador = 3
+    while contador > 0:
+        numRandom = random.randrange(0, 3)
+        jugadasPrograma.append(opcionesJuego[numRandom])
+        contador -= 1
 
-while contador < 3:
-    resultadoRetorn = compararOpciones(jugadasHumano[contador], jugadasPrograma[contador])
-    if resultadoRetorn == -1:
-        puntosProgram += 1
-    elif resultadoRetorn == 1:
-        puntosHuman += 1
-    contador += 1
-print('------- H - P')
-print('Punteo:', puntosHuman, '-', puntosProgram)
+    print('El programa elige:', jugadasPrograma)
 
-if puntosHuman > puntosProgram:
-    print('Ganador: Humano')
-elif puntosProgram > puntosHuman:
-    print('Ganador: Programa')
-else:
-    print('Resultado: Empate')
+    contador = 0
+    puntosHuman = 0
+    puntosProgram = 0
+
+    while contador < 3:
+        resultadoRetorn = compararOpciones(jugadasHumano[contador], jugadasPrograma[contador])
+        if resultadoRetorn == -1:
+            puntosProgram += 1
+        elif resultadoRetorn == 1:
+            puntosHuman += 1
+        contador += 1
+    print('------- H - P')
+    print('Punteo:', puntosHuman, '-', puntosProgram)
+
+    if puntosHuman > puntosProgram:
+        print('Ganador: Humano')
+    elif puntosProgram > puntosHuman:
+        print('Ganador: Programa')
+    else:
+        print('Resultado: Empate')
